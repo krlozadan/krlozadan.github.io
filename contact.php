a75297733807a81d5c57cacef6d65511d2ea7268
Carlos Adan <krlozadan@gmail.com>
2018-11-22 12:27:32 -0800

    Changed sendto email in php file

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'dev@carlosadan.com>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'Demo contact form <demo@domain.com>';
